67cc946f30e61f38c7e064482b1ca1ee5e0944d9
jcvisk <itic18.juan.vazquez@iunis.edu.mx>
2020-12-07 12:52:44 -0600

    agregando y eliminando secciones que no se utilizan

--- prospecto_detalle.php
+++ prospecto_detalle.php
@@ -121,15 +121,12 @@ if (!isset($_SESSION['usuario_admin'])) {
         <div
           class='d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom'>
           <h1 class='h2'>Información general</h1>
+          <?= isset($_SESSION['change_status_successful']) ? $_SESSION['change_status_successful'] : '' ?>
           <div class='btn-toolbar mb-2 mb-md-0'>
             <div class='btn-group mr-2'>
-              <button type='button' class='btn btn-sm btn-outline-secondary'>Share</button>
+              <a href="convert_to_student.php?id=<?= $_GET['id'] ?>" role="button" type='button' class='btn btn-sm btn-outline-secondary'>Admitir</a>
               <a href="excel.php?id=<?= $_GET['id'] ?>" role="button" type='button' class='btn btn-sm btn-outline-secondary'>Exportar</a>
             </div>
-            <button type='button' class='btn btn-sm btn-outline-secondary dropdown-toggle'>
-              <span data-feather='calendar'></span>
-              This week
-            </button>
           </div>
         </div>
         <h2><?= $detalleProspecto['nombre'].' '.$detalleProspecto['apellidopaterno'] ?></h2>
@@ -398,3 +395,4 @@ if (!isset($_SESSION['usuario_admin'])) {
   <script src='assets/js/bootstrap.min.js'></script>
 
 </html>
+<?php borrarErrores(); ?>
